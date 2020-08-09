f874694c376d8c977bc06f20c0221c3b0c7f9973
Josh Rosenberg <josh@josh-rosenberg.com>
2020-08-09 19:25:01 -0400

    get the VPC and networking started

---
+++ networking.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet-a-public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    publicness = "public"
+  }
+}
+
+resource "aws_subnet" "subnet-b-public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    publicness = "public"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "subnet-a-private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.101.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    publicness = "private"
+  }
+}
+
+resource "aws_subnet" "subnet-b-private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.102.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    publicness = "private"
+  }
+}
+

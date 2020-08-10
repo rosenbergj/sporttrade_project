e43f0905c4d026dbf039365b75587d2c394658c9
Josh Rosenberg <josh@josh-rosenberg.com>
2020-08-09 22:16:00 -0400

    better RDS networking

--- networking.tf
+++ networking.tf
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "subnet-a-private" {
+resource "aws_subnet" "subnet_a_private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.101.0/24"
   availability_zone = "us-east-1a"
--- rds.tf
+++ rds.tf
@@ -1,3 +1,8 @@
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.subnet_a_private.id, aws_subnet.subnet_b_private.id]
+}
+
 resource "aws_db_instance" "wp_mysql_db" {
     allocated_storage    = 10
     storage_type         = "gp2"
@@ -9,4 +14,5 @@ resource "aws_db_instance" "wp_mysql_db" {
   # password             = 
     parameter_group_name = "default.mysql5.7"
     multi_az             = true
+    db_subnet_group_name = aws_db_subnet_group.default.id
 }

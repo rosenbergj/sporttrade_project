00ebe43ef0490b1f05376b53fa29933d50f55d0a
Josh Rosenberg <josh@josh-rosenberg.com>
2020-08-10 19:04:12 -0400

    make rds easier to destroy

--- rds.tf
+++ rds.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "wp_mysql_db" {
   password               = "changeme"
   parameter_group_name   = "default.mysql5.7"
   multi_az               = true
+  skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.default.id
   vpc_security_group_ids = [aws_security_group.allow_mysql.id]
 

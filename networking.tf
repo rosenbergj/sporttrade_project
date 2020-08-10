f645b21695c6f315b48449b43a3ac3573095c9f4
Josh Rosenberg <josh@josh-rosenberg.com>
2020-08-09 22:25:56 -0400

    let's add nat GWs

--- networking.tf
+++ networking.tf
@@ -24,6 +24,26 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "natgw_a_eip" {
+  vpc = true
+}
+
+resource "aws_eip" "natgw_b_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "natgw_a" {
+  allocation_id = aws_eip.natgw_a_eip.id
+  subnet_id     = aws_subnet.subnet_a_public.id
+  depends_on    = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "natgw_b" {
+  allocation_id = aws_eip.natgw_b_eip.id
+  subnet_id     = aws_subnet.subnet_b_public.id
+  depends_on    = [aws_internet_gateway.igw]
+}
+
 resource "aws_subnet" "subnet_a_private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.101.0/24"
